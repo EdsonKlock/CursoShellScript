cf15ea0d55c6f09aecca810d4ea98003b977056e
EdsonKlock <131697888+EdsonKlock@users.noreply.github.com>
2023-07-17 14:14:19 -0300

    Add files via upload

---
+++ Untitled-1.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+
+############################################
+#
+# PrimeiroScript.sh - Script de Exemplo do Curso
+#
+# Autor: Edson Klock (edsondefreitasklock@gmail.com)
+# Data Criação: DD/MM/YYYY
+#
+# Descrição: Script de exemplo que le data e hora atual e exibe a lista de alunos
+#
+# Exemplo de uso: ./Primeiroscript.sh
+#
+# Alteracoes
+#        Dia x - Inclusao da Função de ordenação
+#        dia Y - correcao da Função de leitura 
+#
+########################################################
+
+DATAHORA=$(DATA =%H:%M)
+ARQALUNOS="/home/edson/Cursos/ShellScript/arquivos/alunos2.txt
+
+# Função de leitura da data e hora 
+clear
+echo "===== Meu Primeiro Script ======"
+echo ""
+echo "Exibir data e hora atual: $DATAHORA"
+
+# area de Leitura da lista de alunos 
+echo "================================="
+echo "Listagem dos alunos: "
+sort $ARQALUNOS
+~                                                                                                                                                                                             
+~                                                                                                                                                                                             
+~                                                                                                                                                                                             
+~                                                                                                                                                                                             
+~                                                                                                                                                                                             
+~                                                                                                                                                                                             
+"PrimeiroScript.sh" 34L, 847B                                                                                                                                               29,1         Tudo
+
